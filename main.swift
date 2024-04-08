52f85b1184eca1e6053374eb52ad4b720c7443b4
Celina <chabcelina@gmail.com>
2024-04-08 23:05:28 +0200

    adj structure question

---
+++ main.swift
@@ -0,0 +1,15 @@
+// importation nécessaire
+import Foundation
+
+
+// definition d'une structure pour nos question
+struct question {
+    let Question: String
+    let Proposition: [String]
+    let Reponse: Int
+    let Niveau: String
+    let Categorie: String
+}
+
+
+ 
